bab33f8bb52b7b540871e5b0177ed3aaf9575b49
Pedro91Dam <pedroadbmadrid@gmail.com>
2019-04-09 18:45:20 +0200

    Subida con cambio en la rama

--- GitED/src/entornos/nuevogit.java
+++ GitED/src/entornos/nuevogit.java
@@ -4,6 +4,7 @@ public class nuevogit {
 
   public static void main(String[] args) {
     System.out.println("Ya he cambiado la linea concreta de código");
+    System.out.println("Modificacion realizada en la rama desde Eclipse ");
   }
 
 
